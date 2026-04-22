ee8cb62dd295906789865faa647f66c8f72561e1
Rokas Kartenis <rokas.kartenis@cornercasetech.com>
2026-04-22 22:42:13 +0300

    Make navbar adaptive to screen sizes

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,12 +1,11 @@
 import { auth, UserButton } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
-
-import { MainNav } from '@/components/main-nav';
+import { MainNav, MobileNav } from '@/components/main-nav';
 import StoreSwitcher from '@/components/store-switcher';
 import prismadb from '@/lib/prismadb';
 import { ThemeToggle } from '@/components/theme-toggle';
 
-const Navbar = async () => {
+export const Navbar = async () => {
   const { userId } = auth();
 
   if (!userId) {
@@ -21,10 +20,11 @@ const Navbar = async () => {
 
   return (
     <div className="border-b">
-      <div className="flex h-16 items-center px-4">
-        <StoreSwitcher items={stores} />
-        <MainNav className="mx-6" />
-        <div className="ml-auto flex items-center space-x-4">
+      <div className="flex h-16 flex-wrap items-center gap-3 px-4 py-3 xl:flex-nowrap xl:gap-0 xl:py-0">
+        <StoreSwitcher items={stores} className="h-10 min-w-0 flex-1 sm:flex-none xl:w-56" />
+        <MainNav className="mx-6 flex-1" />
+        <div className="ml-auto flex items-center gap-2 sm:gap-3 xl:gap-4">
+          <MobileNav />
           <ThemeToggle />
           <UserButton afterSignOutUrl="/" />
         </div>
@@ -32,5 +32,3 @@ const Navbar = async () => {
     </div>
   );
 };
-
-export default Navbar;

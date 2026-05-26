be96d8c13928a73b917d37a15643cb4ff4a0c60d
Rooooooookas <80284319+N0rmalus@users.noreply.github.com>
2026-05-27 02:11:39 +0300

    Fix product image list display (#53)
    Co-authored-by: Rokas Kartenis <rokas.kartenis@cornercasetech.com>

--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -60,9 +60,9 @@ const ImageUpload = ({ disabled, onChange, onRemove, value }: Props) => {
 
   return (
     <div>
-      <div className={cn('flex items-center gap-4', value.length > 0 && 'mb-4')}>
+      <div className={cn('flex flex-wrap items-center gap-4', value.length > 0 && 'mb-4')}>
         {value.map((url) => (
-          <div key={url} className="relative size-50 overflow-hidden rounded-md">
+          <div key={url} className="relative size-50 shrink-0 overflow-hidden rounded-md">
             <div className="absolute top-2 right-2 z-10">
               <Button type="button" onClick={() => onRemove(url)} variant="destructive" size="icon">
                 <Trash className="h-4 w-4" />
